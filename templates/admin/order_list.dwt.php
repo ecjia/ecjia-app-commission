e1d4bbeae86d639270967060362805f57f0f25ab
宋倩倩 <huangyuyuan@ecmoban.comshuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-09-29 10:56:56 +0800

    闪惠确认核销存入分成数据

--- templates/admin/order_list.dwt.php
+++ templates/admin/order_list.dwt.php
@@ -46,7 +46,7 @@
 
    				 	<!-- {foreach from=$record_list.item item=list} -->
 					<tr>
-						<td>{if $list.order_type eq 1}订单{/if}{if $list.order_type eq 2}<span class="ecjiafc-red">退款</span>{/if}</td>
+						<td>{if $list.order_type eq 1}订单{elseif $list.order_type eq 2}<span class="ecjiafc-red">退款</span>{else}闪惠订单{/if}</td>
 						<td>
 							{assign var=order_url value=RC_Uri::url('orders/admin/info',"order_id={$list.order_id}")}
 						     <a href="{$order_url}" target="_blank">{$list.order_sn}</a>
@@ -60,7 +60,7 @@
 					    <td>￥{$list.total_fee}</td>
 					    <td>{$list.percent_value}%</td>
 						<td>
-							{if $list.order_type eq 1}￥{$list.brokerage_amount}{/if}{if $list.order_type eq 2}<span class="ecjiafc-red">￥{$list.brokerage_amount}</span>{/if}
+							{if $list.order_type eq 1 || $list.order_type eq 3}￥{$list.brokerage_amount}{/if}{if $list.order_type eq 2}<span class="ecjiafc-red">￥{$list.brokerage_amount}</span>{/if}
 						</td>
 						<td>{$list.add_time_formate}</td>
 					</tr>
