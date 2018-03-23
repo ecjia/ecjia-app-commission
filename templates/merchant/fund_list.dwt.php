fa8dde61d4fc531777af580f0e307787f468b82f
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-03-23 15:24:45 +0800

    增加冻结资金显示判断

--- templates/merchant/fund_list.dwt.php
+++ templates/merchant/fund_list.dwt.php
@@ -24,7 +24,9 @@
       	<section class="panel">
       		<div class="panel-body fund-title">
       			<span class="panel-item">账户余额：<span class="price">{$account.formated_money}</span></span>
+      			{if $account.frozen_money neq 0}
       			<span class="panel-item">冻结资金：<span class="price">{$account.formated_frozen_money}</span></span>
+      			{/if}
       			<span class="panel-item">保证金：<span class="price">{$account.formated_deposit}</span></span>
       			<span class="panel-item">可用余额：<span class="price">{$account.formated_amount_available}</span></span>
       			<span class="panel-item"><a class="btn btn-info data-pjax" href='{url path="commission/merchant/reply_fund"}'>申请提现</a></span>
