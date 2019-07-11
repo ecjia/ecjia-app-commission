40a2ad5df089a21eea9835da974515320415cfe5
周瑞丽 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-07-11 13:50:45 +0800

    仅退款退款成功库存还原

--- templates/merchant/fund_list.dwt.php
+++ templates/merchant/fund_list.dwt.php
@@ -54,7 +54,7 @@
                                     {t domain="commission"}提现{/t}
 									{else if $list.change_type eq 'bill'}
                                     {t domain="commission"}结算{/t}
-                                    {else if $list.change_type eq 'order'}
+                                    {else if $list.change_type eq 'order' || $list.change_type eq 'refund'}
                                     {t domain="commission"}采购{/t}
 									{/if}
 									&nbsp;
